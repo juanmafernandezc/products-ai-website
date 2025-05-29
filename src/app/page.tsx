533b82a23161fba2adf7ee0529d11e413ad17fda
Cascalex <acascalesmiranda@gmail.com>
2025-05-29 12:20:43 +0200

    Second Commit Correction II

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import Categories from '../components/Categories'
-import Products from '../components/Products'
-import About from '../components/About'
-import Contact from '../components/Contact'
-import Footer from '../components/Footer'
+import Header from '../../components/Header'
+import Hero from '../../components/Hero'
+import Categories from '../../components/Categories'
+import Products from '../../components/Products'
+import About from '../../components/About'
+import Contact from '../../components/Contact'
+import Footer from '../../components/Footer'
 
 export default function Home(): JSX.Element {
   return (
